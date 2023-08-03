7e5e8d2668cbb0155aaa6bf3936a4757bfad4c7e
LiquidIce95 <davidsanchez062@gmail.com>
2023-08-03 15:08:15 +0200

    Intitial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,jsonify,render_template
+from matplotlib import pyplot as plt
+import mpld3
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5002)
