57791dc4365835ec209faba3001ab3b1c00b93be
LiquidIce95 <davidsanchez062@gmail.com>
2023-08-03 15:11:07 +0200

    initial commit

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
+
     return render_template("index.html")
-## main function
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5002)
