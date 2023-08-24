1f675ee24084d63e9b4258ecbfc9ea98442f5cf9
LiquidIce95 <davidsanchez062@gmail.com>
2023-08-24 20:28:01 +0200

    d

--- OnlineCV2_Dotnet_Angular/backend/MyWebAppBackend/Program.cs
+++ OnlineCV2_Dotnet_Angular/backend/MyWebAppBackend/Program.cs
@@ -19,10 +19,10 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseDeveloperExceptionPage();
+// }
 
 
 app.UseCors("AllowMyOrigin");
