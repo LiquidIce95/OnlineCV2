421ce0f300770f5e2883b69916808b761c6d817e
LiquidIce95 <davidsanchez062@gmail.com>
2023-09-06 20:55:43 +0200

    d

--- OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/dot-simulation/dot-simulation.component.ts
+++ OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/dot-simulation/dot-simulation.component.ts
@@ -27,7 +27,7 @@ export class DotSimulationComponent implements AfterViewInit {
   }
 
   private initializeDots() {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 70; i++) {
       this.dots.push({
         x: Math.random() * window.innerWidth,
         y: Math.random() * window.innerHeight,
@@ -45,6 +45,7 @@ export class DotSimulationComponent implements AfterViewInit {
       this.edgeCollision(dot);
       this.drawDot(dot);
     });
+    this.dotInteraction();
     requestAnimationFrame(this.animate);
   }
 
@@ -62,5 +63,33 @@ export class DotSimulationComponent implements AfterViewInit {
     this.ctx.beginPath();
     this.ctx.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
     this.ctx.fill();
+  }
+
+  private dotInteraction() {
+    const proximityThreshold = 15 * (window.innerHeight / 100);  // 15vh
+
+    for (let i = 0; i < this.dots.length; i++) {
+      for (let j = i + 1; j < this.dots.length; j++) {
+        const dx = this.dots[i].x - this.dots[j].x;
+        const dy = this.dots[i].y - this.dots[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // Dot collision
+        const minDist = this.dots[i].size + this.dots[j].size;
+        if (distance < minDist) {
+          [this.dots[i].dx, this.dots[j].dx] = [this.dots[j].dx, this.dots[i].dx];
+          [this.dots[i].dy, this.dots[j].dy] = [this.dots[j].dy, this.dots[i].dy];
+        }
+
+        // Draw line if close enough
+        if (distance < proximityThreshold) {  // 3vh
+          this.ctx.beginPath();
+          this.ctx.moveTo(this.dots[i].x, this.dots[i].y);
+          this.ctx.lineTo(this.dots[j].x, this.dots[j].y);
+          this.ctx.strokeStyle = `rgba(0, 0, 0, ${1 - distance / proximityThreshold})`;  // Fade-in effect
+          this.ctx.stroke();
+        }
+      }
+    }
   }
 }
