c3b42b72ad14188a5464e34e75d44faec9face8f
LiquidIce95 <davidsanchez062@gmail.com>
2023-09-07 13:22:35 +0200

    d

--- OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/dot-simulation/dot-simulation.component.ts
+++ OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/dot-simulation/dot-simulation.component.ts
@@ -7,8 +7,10 @@ interface Dot {
   dy: number;
   size: number;
   isStatic?: boolean;
-
 }
+
+const PROXIMITY_THRESHOLD_RATIO = 0.17;
+const DOT_COUNT = 70;
 
 @Component({
   selector: 'app-dot-simulation',
@@ -18,12 +20,14 @@ interface Dot {
 export class DotSimulationComponent implements AfterViewInit {
   @ViewChild('canvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
   private ctx!: CanvasRenderingContext2D;
-  dots: Dot[] = [];  // Property 'dots' declared and initialized here
+  dots: Dot[] = [];
   private staticDotStartCoords: { x: number, y: number } | null = null;
-  private isAiming : Boolean = false;
-  private X : number = 0;
-  private Y : number = 0;
+  private isAiming: boolean = false;
+  private X: number = 0;
+  private Y: number = 0;
   private dot: Dot | null = null;
+  
+  private proximityThreshold: number = PROXIMITY_THRESHOLD_RATIO * window.innerHeight;
 
   ngAfterViewInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d')!;
@@ -31,99 +35,79 @@ export class DotSimulationComponent implements AfterViewInit {
     this.canvas.nativeElement.height = window.innerHeight;
     this.initializeDots();
     requestAnimationFrame(this.animate);
-
     this.canvas.nativeElement.addEventListener('mousedown', this.mouseDown);
   }
 
-  // Invariant: only one dot is static at a time
+  private initializeDots() {
+    for (let i = 0; i < DOT_COUNT; i++) {
+      this.dots.push(this.createRandomDot());
+    }
+  }
+
+  private createRandomDot(): Dot {
+    return {
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      dx: (Math.random() - 0.5) * 2,
+      dy: (Math.random() - 0.5) * 2,
+      size: Math.random() * 4 + 2,
+      isStatic: false,
+    };
+  }
+
   private mouseDown = (event: MouseEvent) => {
-    console.log("test");
     const x = event.clientX;
     const y = event.clientY;
-  
-      
-
 
     for (let i = 0; i < this.dots.length; i++) {
+      const distance = this.getDistance(x, y, this.dots[i].x, this.dots[i].y);
       
-      const dx = x - this.dots[i].x;
-      const dy = y - this.dots[i].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.dots[i].size * 1 && this.dots[i].isStatic == false){
-        // Enter static mode on the first click
-        this.dots[i].isStatic = true;
-        this.staticDotStartCoords = { x, y };  // Store start coords
-        this.dots[i].x = x;
-        this.dots[i].y = y;
-        this.isAiming = true;
-        this.dot = this.dots[i];
-        this.canvas.nativeElement.addEventListener('mousemove', this.mouseMove);
-
+      if (distance < this.dots[i].size * 1 && !this.dots[i].isStatic) {
+        this.enterStaticMode(i, x, y);
+      } else if (this.dots[i].isStatic) {
+        this.exitStaticMode(i, x, y);
       }
-  
-      else if (this.dots[i].isStatic) {
-        // Set velocity based on the second click for static dot
-        this.dots[i].dx = (x - this.staticDotStartCoords!.x)/50;
-        this.dots[i].dy = (y - this.staticDotStartCoords!.y)/50;
-        this.dots[i].isStatic = false;  // Exit static mode
-        this.staticDotStartCoords = null;  // Reset start coords
-        this.canvas.nativeElement.removeEventListener('mousemove', this.mouseMove);
-        this.isAiming = false;
-        this.dot =null
-        this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-      } 
-      
-      // break;
     }
   }
 
-  
-
-  private initializeDots() {
-    for (let i = 0; i < 70; i++) {
-      this.dots.push({
-        x: Math.random() * window.innerWidth,
-        y: Math.random() * window.innerHeight,
-        dx: (Math.random() - 0.5) * 2,
-        dy: (Math.random() - 0.5) * 2,
-        size: Math.random() * 4 + 2,
-        isStatic: false
-      });
-    }
+  private enterStaticMode(index: number, x: number, y: number) {
+    this.dots[index].isStatic = true;
+    this.staticDotStartCoords = { x, y };
+    this.dots[index].x = x;
+    this.dots[index].y = y;
+    this.isAiming = true;
+    this.dot = this.dots[index];
+    this.canvas.nativeElement.addEventListener('mousemove', this.mouseMove);
   }
 
-  drawAim(){
-    if(this.dot != null){
-      this.ctx.beginPath();
-      this.ctx.moveTo(this.dot.x, this.dot.y);
-      this.ctx.lineTo(this.X, this.Y);
-      this.ctx.strokeStyle = `rgba(100, 0, 0)`;  // Fade-in effect
-      this.ctx.stroke();
-    }
-
-
+  private exitStaticMode(index: number, x: number, y: number) {
+    this.dots[index].dx = (x - this.staticDotStartCoords!.x)/50;
+    this.dots[index].dy = (y - this.staticDotStartCoords!.y)/50;
+    this.dots[index].isStatic = false;
+    this.staticDotStartCoords = null;
+    this.canvas.nativeElement.removeEventListener('mousemove', this.mouseMove);
+    this.isAiming = false;
+    this.dot = null;
   }
 
-  mouseMove = (event: MouseEvent) => {
-    if(this.isAiming && this.dot !=null){
-
-      this.X = event.clientX;
-      this.Y = event.clientY;
-
-      this.drawAim();
-    }
+  private getDistance(x1: number, y1: number, x2: number, y2: number): number {
+    const dx = x1 - x2;
+    const dy = y1 - y2;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 
   private animate = () => {
     this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    
     this.dots.forEach(dot => {
-      this.moveDot(dot);  // Pass the stored mouse event
+      this.moveDot(dot);
       this.edgeCollision(dot);
       this.drawDot(dot);
     });
+    
     this.dotInteraction();
     this.drawAim();
+    
     requestAnimationFrame(this.animate);
   }
 
@@ -131,8 +115,7 @@ export class DotSimulationComponent implements AfterViewInit {
     if (!dot.isStatic) { 
       dot.x += dot.dx;
       dot.y += dot.dy;
-    } 
-    
+    }
   }
 
   private edgeCollision(dot: Dot) {
@@ -147,30 +130,46 @@ export class DotSimulationComponent implements AfterViewInit {
   }
 
   private dotInteraction() {
-    const proximityThreshold = 17 * (window.innerHeight / 100);  // 15vh
-
     for (let i = 0; i < this.dots.length; i++) {
       for (let j = i + 1; j < this.dots.length; j++) {
-        const dx = this.dots[i].x - this.dots[j].x;
-        const dy = this.dots[i].y - this.dots[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+        const distance = this.getDistance(this.dots[i].x, this.dots[i].y, this.dots[j].x, this.dots[j].y);
 
-        // Dot collision
-        const minDist = this.dots[i].size + this.dots[j].size;
-        if (distance < minDist) {
-          [this.dots[i].dx, this.dots[j].dx] = [this.dots[j].dx, this.dots[i].dx];
-          [this.dots[i].dy, this.dots[j].dy] = [this.dots[j].dy, this.dots[i].dy];
-        }
-
-        // Draw line if close enough
-        if (distance < proximityThreshold) {  // 3vh
-          this.ctx.beginPath();
-          this.ctx.moveTo(this.dots[i].x, this.dots[i].y);
-          this.ctx.lineTo(this.dots[j].x, this.dots[j].y);
-          this.ctx.strokeStyle = `rgba(0, 0, 0, ${1 - distance / proximityThreshold})`;  // Fade-in effect
-          this.ctx.stroke();
-        }
+        this.handleDotCollision(i, j, distance);
+        this.drawLineIfCloseEnough(i, j, distance);
       }
     }
+  }
+
+  private handleDotCollision(i: number, j: number, distance: number) {
+    const minDist = this.dots[i].size + this.dots[j].size;
+    if (distance < minDist) {
+      [this.dots[i].dx, this.dots[j].dx] = [this.dots[j].dx, this.dots[i].dx];
+      [this.dots[i].dy, this.dots[j].dy] = [this.dots[j].dy, this.dots[i].dy];
+    }
+  }
+
+  private drawLineIfCloseEnough(i: number, j: number, distance: number) {
+    if (distance < this.proximityThreshold) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.dots[i].x, this.dots[i].y);
+      this.ctx.lineTo(this.dots[j].x, this.dots[j].y);
+      this.ctx.strokeStyle = `rgba(0, 0, 0, ${1 - distance / this.proximityThreshold})`;
+      this.ctx.stroke();
+    }
+  }
+
+  drawAim() {
+    if(this.dot) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.dot.x, this.dot.y);
+      this.ctx.lineTo(this.X, this.Y);
+      this.ctx.strokeStyle = 'rgba(100, 0, 0, 0.8)';
+      this.ctx.stroke();
+    }
+  }
+
+  private mouseMove = (event: MouseEvent) => {
+    this.X = event.clientX;
+    this.Y = event.clientY;
   }
 }
