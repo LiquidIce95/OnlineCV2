dc406e601b76769e675e30bea9799783f297b9e8
LiquidIce95 <davidsanchez062@gmail.com>
2023-09-11 14:51:51 +0200

    d

--- OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/freelancing/freelancing.component.ts
+++ OnlineCV2_Dotnet_Angular/frontend/OCV2/src/app/freelancing/freelancing.component.ts
@@ -13,19 +13,18 @@ import { trigger, state, style, animate, transition, sequence } from '@angular/a
         height: '0', 
         width: '0',  // Added width
         opacity: '0', 
-        overflow: 'hidden' 
       })),
       
       transition('void => *', [
         sequence([
-          animate('0.5s ease-in-out', style({ height: '*', width: '*', opacity: '0', overflow: 'hidden' })),
+          animate('0.5s ease-in-out', style({ height: '*', width: '*', opacity: '0' })),
           animate('0.5s ease-in-out', style({ opacity: '1' }))
         ])
       ]), 
       transition('* => void', [
         sequence([
           animate('0.5s ease-in-out', style({ opacity: '0' })),
-          animate('0.5s ease-in-out', style({ height: '0', width: '0', opacity: '0', overflow: 'hidden' })),
+          animate('0.5s ease-in-out', style({ height: '0', width: '0', opacity: '0' })),
         ])
       ]), 
     ])
